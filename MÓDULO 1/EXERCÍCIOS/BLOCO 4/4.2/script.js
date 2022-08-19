4d1d1627d28d55943d1475268b5885781067608d
elaurentium <credopuke@icloud.com>
2022-08-19 19:22:23 -0300

    exercicios bloco 4.2

---
+++ MÓDULO 1/EXERCÍCIOS/BLOCO 4/4.2/script.js
@@ -0,0 +1,59 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// console.log(numbers);
+
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+    //     sum += numbers[i];
+    // }
+
+// console.log(sum);
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     sum += numbers[i] / numbers.length;
+// }
+// if (sum > 20) {
+//     console.log('valor maior que 20')
+// } else {
+//     console.log('valor menor ou igual a 20');
+// }
+
+// console.log(sum);
+
+// let maior = 0;
+// // let numbers = [2, 3, 5];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     // console.log('esse é o número maior', maior);
+//     if (maior < numbers[i]) {
+//         maior = numbers[i];
+//         // console.log(maior);
+//     }
+// }
+
+// console.log(maior);
+
+// let resulta = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 !== 0) {
+//         resulta += 1;
+//     }
+// }
+
+// if (resulta === 0) {
+//     console.log('valor maior que 20');
+// } else {
+//     console.log(result);
+// }
+
+// let menor = numbers[0];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (menor > numbers[i]) {
+//         menor = numbers[i];
+//     }
+// }
+
+// console.log(menor);
