793e814508f0071a7c58ef62144f6dee01da0d51
elaurentium <credopuke@icloud.com>
2022-08-22 19:36:57 -0300

    exercicios do dia

---
+++ MÓDULO 1/EXERCÍCIOS/BLOCO 4/4.2/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,54 @@
+// let fatorial = 1;
+
+// for (let index = 10; index > 0; index -= 1) {
+//     fatorial *= index;
+// }
+
+// console.log(fatorial);
+
+
+// let word = 'tryber';
+// let reverso = '';
+
+// for (let index = word.length - 1; index >= 0; index -= 1) {
+//     reverso += word[index];
+// }
+
+// console.log(reverso);
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let maior = array[0];
+// let menor = array[0];
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if(array[index] > maior) {
+//         maior = array[index];  
+//     }
+// }
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if(array[index] < menor) {
+//         menor = array[index];
+//     }
+// }
+
+// console.log(maior);
+// console.log(menor);
+
+
+// let numeros = 0;
+
+// for (let index = 2; index <= 50; index += 1) {
+//     let isPrimo = true;
+// for (let index1 = 2; index1 < index; index += 1) {
+//     if (index % index1 === 0) {
+//         isPrimo = false;
+//     }
+// }
+// if (isPrimo) {
+//     numeros = index;
+// }
+// }
+
+// console.log(numeros);
