b652baa87c05110b3c9c432db7fd87801d9646fc
elaurentium <credopuke@icloud.com>
2022-08-23 18:12:27 -0300

    exercicios do dia 4.4

---
+++ MÓDULO 1/EXERCÍCIOS/BLOCO 4/4.2/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios 4.4/script.js
@@ -0,0 +1,34 @@
+// let player = {
+// name : 'Marta',
+// lastName : 'Silva',
+// age : 34,
+// medals : { golden: 2, silver: 3 }
+// };
+
+// // console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' tem ' + player['age'] + ' anos de idade.');
+// player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+// // console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes ' + player['bestInTheWorld']);
+// let primeiraMedalha = player[0];
+// let segundaMedalha = player[1];
+// console.log('A jogadora ' + player[0] + ' ' + ' possui e ' + player[1]);
+
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for (let index in names) {
+//     console.log('Olá, ' + names[index]);
+//   }
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+
+//   for(let index in car) {
+//     console.log(car[index]);
+//   }
